76d7ff169413363fc2c5cb12b1dda02faf9d1964
Wendy Vu <wendyvuhoo@gmail.com>
2023-01-25 05:56:52 -0800

    added timer, and also taking off time if question is answered wrong

--- Assets/script.js
+++ Assets/script.js
@@ -18,11 +18,11 @@ var questions = [
     },
     {
         question: "In which HTML element would we put JavaScript?",
-        choice1: "js",
-        choice2: "script",
+        choice1: "js tag",
+        choice2: "script tag",
         choice3: "javascript",
-        choice4: "head", 
-        answer: 3 
+        choice4: "head tag", 
+        answer: 2 
     },
     {
         question: "If we want a message to appear in the console, which of the below is the correct option?",
@@ -35,7 +35,7 @@ var questions = [
     {
         question: "Where in HTML would we have the link to CSS?",
         choice1: "At the very end",
-        choice2: "In the Head element>",
+        choice2: "In the Head element",
         choice3: "In the Body element",
         choice4: "Anywhere is fine",
         answer: 2 
@@ -48,9 +48,29 @@ function startGame () {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestions();
+    setTime();
 };
+
+var secondsLeft = 90;
+var timeEl = document.querySelector(".time");
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left to answer all the questions";
+
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        return window.location.assign("./end.html"); 
+      }
+  
+    }, 1000);
+  }
 
 function getNewQuestions() {
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
@@ -72,6 +92,8 @@ function getNewQuestions() {
 
 };
 
+var correctness = document.querySelector(".correctness");
+
 choices.forEach(choice => {
     choice.addEventListener("click", e => {
         if(!acceptingAnswers) return;
@@ -79,6 +101,21 @@ choices.forEach(choice => {
         acceptingAnswers = false; 
         var selectedChoice = e.target;
         var selectedAnswer = selectedChoice.dataset["number"];
+
+        var answerCorrectness = "incorrect";
+
+        if (selectedAnswer == currentQuestion.answer) {
+            answerCorrectness = "correct"; 
+        } else if (answerCorrectness = "inccorect") { 
+            secondsLeft -= 15;
+            correctness.textContent = "Remember! -15 seconds for each wrong answer"
+        }
+        
+        console.log(answerCorrectness);
+
+        selectedChoice.parentElement.classList.add(answerCorrectness);
+
+
         getNewQuestions(); 
     });
 });
